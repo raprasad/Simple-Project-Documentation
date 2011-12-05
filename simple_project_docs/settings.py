524a3f4d117b337264b430c736a9ab4b0b4de4b3
Raman Prasad <raman_prasad@harvard.edu>
2011-12-05 12:34:29 -0500

    update to settings

--- simple_project_docs/settings.py
+++ simple_project_docs/settings.py
@@ -17,8 +17,8 @@ SESSION_COOKIE_NAME = 'simple_docs'
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
-SMTP_CONNECTION_STRING = 'mail.fas.harvard.edu' 
-EMAIL_HOST = 'mail.fas.harvard.edu' 
+SMTP_CONNECTION_STRING = config.SMTP_CONNECTION_STRING
+EMAIL_HOST = config.EMAIL_HOST
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
